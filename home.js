14425a9613deb145965d30c806cf79858fd9dd1e
Dilan Udawattha <87587934+Dil2010@users.noreply.github.com>
2022-06-15 19:02:54 -0700

    First Commit

---
+++ home.js
@@ -0,0 +1,15 @@
+console.log('Hello');
+console.log('Hello world');
+// alert('this is Dilan');
+
+var b='smoothee';
+console.log(b);
+
+var somenumber=45;
+console.log(somenumber);
+
+var age=prompt('What is your age?');
+
+document.getElementById('someText').innerHTML=age;
+
+
